d48ea7dd1708a9ac6271eeea3f72dd71bd86f4b9
doridc06 <a_dorianaangelica.dacostamagello@iespablopicasso.es>
2024-01-22 21:18:10 +0100

    haciendo peliculas.. y q me funcionara conectara la api y no paso :/

---
+++ Dorairo/src/main/java/models/Pelicula.java
@@ -0,0 +1,55 @@
+package models;
+
+/**
+* Clase que representa una película
+* 
+* @author Doriana Dc
+*
+*/
+public class Pelicula {
+
+// Atributos de la película
+private String title;
+private String overview;
+private String release_date;
+private String poster_path;
+
+/**
+* Obtener el resumen de la película
+* 
+* @return
+*/
+public String getOverview() {
+ return overview;
+}
+
+
+/**
+* Obtener la ruta del póster de la película
+* 
+* @return
+*/
+public String getPoster_path() {
+ return poster_path;
+}
+
+
+/**
+* Obtener la fecha de estreno de la película
+* 
+* @return
+*/
+public String getRelease_date() {
+ return release_date;
+}
+
+
+/**
+* Obtener el título de la película
+* 
+* @return
+*/
+public String getTitle() {
+ return title;
+}
+}
--- Dorairo/src/main/java/module-info.java
+++ Dorairo/src/main/java/module-info.java
@@ -3,7 +3,10 @@ module Dorairo {
 	requires javafx.fxml;
 	requires javafx.base;
 	requires javafx.graphics;
+	requires com.google.gson;
+	requires okhttp3;
+	opens controller to com.google.gson; 
 
 	opens application to javafx.graphics, javafx.fxml;
-	opens controller to javafx.fxml;
+	opens models to javafx.fxml;
 }

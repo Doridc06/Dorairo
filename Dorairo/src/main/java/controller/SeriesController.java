8afe9c8a4daa2a8dd440e1fc9eea0b5b8082bf20
jairopo <jairo.po8@gmail.com>
2024-01-23 13:44:20 +0100

    Modificando peliculas y serie cabecera

--- Dorairo/src/main/java/controller/SeriesController.java
+++ Dorairo/src/main/java/controller/SeriesController.java
@@ -3,11 +3,21 @@ package controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import constants.Constants;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.control.CheckBox;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import utilities.GestorVentanas;
 
 public class SeriesController {
-	
+
     @FXML
     private ResourceBundle resources;
 
@@ -45,18 +55,78 @@ public class SeriesController {
     private CheckBox GeneroTerror;
 
     @FXML
-    void initialize() {
-        assert GeneroAccion != null : "fx:id=\"GeneroAccion\" was not injected: check your FXML file 'Series.fxml'.";
-        assert GeneroAnimacion != null : "fx:id=\"GeneroAnimacion\" was not injected: check your FXML file 'Series.fxml'.";
-        assert GeneroAventura != null : "fx:id=\"GeneroAventura\" was not injected: check your FXML file 'Series.fxml'.";
-        assert GeneroCienciaFiccion != null : "fx:id=\"GeneroCienciaFiccion\" was not injected: check your FXML file 'Series.fxml'.";
-        assert GeneroComedia != null : "fx:id=\"GeneroComedia\" was not injected: check your FXML file 'Series.fxml'.";
-        assert GeneroDrama != null : "fx:id=\"GeneroDrama\" was not injected: check your FXML file 'Series.fxml'.";
-        assert GeneroMisterio != null : "fx:id=\"GeneroMisterio\" was not injected: check your FXML file 'Series.fxml'.";
-        assert GeneroMusical != null : "fx:id=\"GeneroMusical\" was not injected: check your FXML file 'Series.fxml'.";
-        assert GeneroSuspenso != null : "fx:id=\"GeneroSuspenso\" was not injected: check your FXML file 'Series.fxml'.";
-        assert GeneroTerror != null : "fx:id=\"GeneroTerror\" was not injected: check your FXML file 'Series.fxml'.";
+    private ImageView imagenLogoCabecera;
 
+    @FXML
+    private Pane paneCabecera;
+
+    @FXML
+    private AnchorPane paneFondo;
+
+    @FXML
+    private StackPane stackPaneInicioCabecera;
+
+    @FXML
+    private StackPane stackPaneLogoCabecera;
+
+    @FXML
+    private StackPane stackPanePeliculasCabecera;
+    
+    /** Scene de la ventana de Inicio */
+  	private Scene scene;
+
+  	/** Stage de la ventana de Inicio */
+  	private Stage stage;
+
+  	/** Instancia del gestor de ventanas **/
+  	private GestorVentanas gestorVentanas;
+
+    @FXML
+    void initialize() {
+   // Inicializamos el Gestor de ventanas
+  		gestorVentanas = new GestorVentanas();
+  		// Establece la imagen del logo
+  		Image imagenLogo = new Image(getClass().getResourceAsStream(Constants.URL_LOGO_AMPLIADO));
+  		imagenLogoCabecera.setImage(imagenLogo);
     }
+    
+    @FXML
+  	void inicioClicked(MouseEvent event) {
+  		setSceneAndStage();
+  		gestorVentanas.muestraVentana(stage, Constants.URL_INICIO_FXML, "Inicio");
+  	}
+
+  	@FXML
+  	void peliculasClicked(MouseEvent event) {
+  		setSceneAndStage();
+  		gestorVentanas.muestraVentana(stage, Constants.URL_PELICULA_FXML, "Pelicula");
+  	}
+
+  	@FXML
+  	void seriesClicked(MouseEvent event) {
+  		setSceneAndStage();
+  		gestorVentanas.muestraVentana(stage, Constants.URL_SERIES_FXML, "Series");
+  	}
+
+  	@FXML
+  	void buscadorClicked(MouseEvent event) {
+  		setSceneAndStage();
+  		gestorVentanas.muestraVentana(stage, Constants.URL_BUSCADOR_FXML, "Buscador");
+  	}
+
+  	@FXML
+  	void perfilClicked(MouseEvent event) {
+  		setSceneAndStage();
+  		gestorVentanas.muestraVentana(stage, Constants.URL_PERFIL_FXML, "Perfil");
+  	}
+
+  	/**
+  	 * Asigna los valores correspondientes del stage y el scene
+  	 * 
+  	 */
+  	public void setSceneAndStage() {
+  		scene = imagenLogoCabecera.getScene();
+  		stage = (Stage) scene.getWindow();
+  	}
 
 }

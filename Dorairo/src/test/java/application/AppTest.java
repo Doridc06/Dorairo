8c61bb5fc0f8a544d5b897651e8d424564566466
jairopo <jairo.po8@gmail.com>
2024-02-14 14:16:27 +0100

    Junit modificado y pom

--- Dorairo/src/test/java/application/AppTest.java
+++ Dorairo/src/test/java/application/AppTest.java
@@ -1,8 +1,9 @@
 package application;
 
 import java.io.IOException;
+import java.util.Date;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.testfx.api.FxAssert;
 import org.testfx.api.FxRobot;
@@ -11,19 +12,30 @@ import org.testfx.framework.junit5.Start;
 import org.testfx.matcher.base.NodeMatchers;
 import org.testfx.matcher.base.WindowMatchers;
 
+import conexion.HibernateUtil;
+import dao.UsuarioDaoImpl;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import models.Usuario;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @ExtendWith(ApplicationExtension.class)
 class AppTest {
 
 	private Stage stage;
 	private Parent mainNode;
+	private Usuario usuarioTest = new Usuario("UsuarioPrueba", "Enrique Martínez", "enrique@ejemplo.com",
+			"1234", new Date());
 
 	@Start
 	public void start(Stage primaryStage) {
+		// Comprueba si existe el usuario para eliminarlo
+		UsuarioDaoImpl uDao = new UsuarioDaoImpl(HibernateUtil.openSession());
+		if (uDao.searchByUsuario(usuarioTest.getUser()) != null) {
+			uDao.delete(usuarioTest);
+		}
 		stage = primaryStage;
 		try {
 			mainNode = FXMLLoader.load(getClass().getResource("/view/Login.fxml"));
@@ -36,14 +48,15 @@ class AppTest {
 	}
 
 	@Test
+	@Order(1)
 	void testUsuarioNoExiste() {
 		// Crea el FxRobot
 		FxRobot fxRobot = new FxRobot();
-		// Introduce un usuario y contraseña inexistentes
+		// Introduce un usuario y contraseña de una persona no registrada
 		fxRobot.clickOn("#txtUsuario");
-		fxRobot.write("Usuario");
+		fxRobot.write(usuarioTest.getUser());
 		fxRobot.clickOn("#pwContrasena");
-		fxRobot.write("password");
+		fxRobot.write(usuarioTest.getClave());
 		// Le da al boton de logearse
 		fxRobot.clickOn("#btnLogin");
 		// Verifica que salta la alerta de error
@@ -53,14 +66,41 @@ class AppTest {
 	}
 
 	@Test
+	@Order(2)
+	void testRegistroFunciona() {
+		// Crea el FxRobot
+		FxRobot fxRobot = new FxRobot();
+		// Abre la pantalla de registro
+		fxRobot.clickOn("#lblPulsaAqui");
+		// Introduce los datos
+		fxRobot.clickOn("#txtUsuario");
+		fxRobot.write(usuarioTest.getUser());
+		fxRobot.clickOn("#txtCorreo");
+		fxRobot.write(usuarioTest.getCorreo());
+		fxRobot.clickOn("#txtNombre");
+		fxRobot.write(usuarioTest.getNombre());
+		fxRobot.clickOn("#pwContrasena");
+		fxRobot.write(usuarioTest.getClave());
+		fxRobot.clickOn("#pwRepetirContrasena");
+		fxRobot.write(usuarioTest.getClave());
+		// Presiona el boton de registro
+		fxRobot.clickOn("#btnRegistrarse");
+		// Verifica que salta la alerta de informacion
+		FxAssert.verifyThat("#alertaInformacion", NodeMatchers.isEnabled());
+		// Le da al boton de Aceptar para cerrar la alerta y ventana
+		fxRobot.clickOn("#btnAceptar");
+	}
+
+	@Test
+	@Order(3)
 	void testUsuarioExiste() {
 		// Crea el FxRobot
 		FxRobot fxRobot = new FxRobot();
-		// Introduce un usuario y contraseña existentes
+		// Introduce un usuario y contraseña de una persona registrada
 		fxRobot.clickOn("#txtUsuario");
-		fxRobot.write(".");
+		fxRobot.write(usuarioTest.getUser());
 		fxRobot.clickOn("#pwContrasena");
-		fxRobot.write(".");
+		fxRobot.write(usuarioTest.getClave());
 		// Le da al boton de logearse
 		fxRobot.clickOn("#btnLogin");
 		// Verifica que salta la alerta de informacion
@@ -71,19 +111,4 @@ class AppTest {
 		FxAssert.verifyThat(fxRobot.window("Inicio"), WindowMatchers.isShowing());
 	}
 
-	@Test
-	void testRegistroFunciona() {
-		// Crea el FxRobot
-		FxRobot fxRobot = new FxRobot();
-		// Introduce un nombre y correo no existentes
-		fxRobot.clickOn("#txtUsuario");
-		fxRobot.write("Enrique");
-		fxRobot.clickOn("#txtCorreo");
-		fxRobot.write("enrique@ejemplo.com");
-		fxRobot.clickOn("#txtNombre");
-		fxRobot.write("Enrique Martínez Fernández");
-		fxRobot.clickOn("#pwContrasena");
-		fxRobot.write("enrique");
-		// terminar
-	}
 }

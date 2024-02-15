fbc0327cd1dc54df19fd2a1d78dbbca563dbf3c7
Doridc06 <a_dorianaangelica.dacostamagello@iespablopicasso.es>
2024-02-15 20:49:59 +0100

    ..

--- Dorairo/src/main/java/utilities/Utils.java
+++ Dorairo/src/main/java/utilities/Utils.java
@@ -25,216 +25,280 @@ import javafx.stage.Stage;
  */
 public class Utils {
 
-	/** Stage principal */
-	private static Stage ownerStage;
+  /** Stage principal */
 
-	/**
-	 * Private Utils Constructor
-	 */
-	private Utils() {
-	}
+  private static Stage ownerStage;
 
-	/**
-	 * Muestra un dialog con el mensaje proporcionado segun el tipo de alerta
-	 * 
-	 * @param mensaje a mostrar en el dialog
-	 * @param tipo    de la alerta
-	 */
-	public static void mostrarAlerta(String mensaje, String tipo) {
-		Alert alert = new Alert(AlertType.valueOf(tipo));
-		alert.setTitle(tipo);
-		alert.setContentText(mensaje);
-		alert.initOwner(ownerStage);
-		Button boton = (Button) alert.getDialogPane().lookupButton(ButtonType.OK);
-		boton.setId("btnAceptar");
-		boton.setText("Aceptar");
-		if (tipo.equalsIgnoreCase(Constants.ERROR_TYPE)) {
-			alert.getDialogPane().setId("alertaError");
-		} else if (tipo.equalsIgnoreCase(Constants.INFORMATION_TYPE)) {
-			alert.getDialogPane().setId("alertaInformacion");
-		}
-		alert.showAndWait();
-	}
+  /**
+   * 
+   * Private Utils Constructor
+   * 
+   */
 
-	/**
-	 * Establece el stage proporcionado como ownerStage
-	 * 
-	 * @param stage que se va a establecer
-	 */
-	public static void setOwnerStage(Stage stage) {
-		ownerStage = stage;
-	}
+  private Utils() {
 
-	/**
-	 * Habilita la busqueda de la foto por los ficheros
-	 * 
-	 * @param stage
-	 */
-	public static String buscarFotoArchivos(Stage stage) {
-		try {
-			FileChooser fileChooser = new FileChooser();
-			fileChooser.setTitle("Buscar Imagen");
+  }
 
-			// Agrega filtros para facilitar la busqueda
-			fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("JPG", "*.jpg"),
-					new FileChooser.ExtensionFilter("PNG", "*.png"), new FileChooser.ExtensionFilter("JFIF", "*.jfif"));
+  /**
+   * 
+   * Muestra un dialog con el mensaje proporcionado segun el tipo de alerta
+   *
+   * 
+   * 
+   * @param mensaje Mensaje a mostrar en el dialog
+   * 
+   * @param tipo Tipo de la alerta
+   * 
+   */
 
-			// Obtiene la imagen seleccionada
-			return fileChooser.showOpenDialog(stage).getAbsolutePath();
+  public static void mostrarAlerta(String mensaje, String tipo) {
+    Alert alert = new Alert(AlertType.valueOf(tipo));
+    alert.setTitle(tipo);
+    alert.setContentText(mensaje);
+    alert.initOwner(ownerStage);
+    Button boton = (Button) alert.getDialogPane().lookupButton(ButtonType.OK);
+    boton.setId("btnAceptar");
+    boton.setText("Aceptar");
+    if (tipo.equalsIgnoreCase(Constants.ERROR_TYPE)) {
+      alert.getDialogPane().setId("alertaError");
+    } else if (tipo.equalsIgnoreCase(Constants.INFORMATION_TYPE)) {
+      alert.getDialogPane().setId("alertaInformacion");
+    } else {
+      alert.getDialogPane().setId("alertaWarning");
+    }
+    alert.showAndWait();
 
-		} catch (Exception e) {
-			mostrarAlerta("Error al seleccionar una foto", Constants.ERROR_TYPE);
-			return null;
-		}
-	}
+  }
 
-	/**
-	 * Comprueba que la contraseña y la repetición sean iguales
-	 * 
-	 * @param contrasena
-	 * @param repeticionContrasena
-	 * @return True si coinciden; false si son distintas
-	 */
-	public static boolean compruebaContrasenas(String contrasena, String repeticionContrasena) {
-		if (contrasena.compareTo(repeticionContrasena) == 0) {
-			if (contrasena.length() > 0 && contrasena.length() <= 20) {
-				return true;
-			} else {
-				Utils.mostrarAlerta("La contraseña no puede ser mayor de 20 caracteres (ni estar vacía).",
-						Constants.WARNING_TYPE);
-				return false;
-			}
-		} else {
-			Utils.mostrarAlerta("Las constraseñas no coinciden.", Constants.WARNING_TYPE);
-			return false;
-		}
-	}
+  /**
+   * 
+   * Establece el stage proporcionado como ownerStage
+   *@param stage que se va a establecer
+   * 
+   */
 
-	/**
-	 * Muestra una alerta de confirmacion
-	 * 
-	 * @return True si se pulsa aceptar; False si se pulsa cancelar
-	 */
-	public static boolean confirmacion() {
-		// Crea una alerta de tipo confirmacion
-		Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-		alert.setHeaderText(null);
-		alert.setTitle("Confirmación");
-		alert.initOwner(ownerStage);
-		alert.setContentText("¿Estás seguro de querer realizar la acción?");
-		// Muestra la alerta y espera hasta que se cierre
-		Optional<ButtonType> result = alert.showAndWait();
+  public static void setOwnerStage(Stage stage) {
 
-		// Comprueba si se ha pulsado el boton de ok y lo envia
-		return result.isPresent() && result.get() == ButtonType.OK;
-	}
+    ownerStage = stage;
 
-	/**
-	 * Genera un nuevo id para las peliculas
-	 * 
-	 * @return Nuevo id
-	 */
-	public static Integer generaMovieId() {
-		PeliculaDaoImpl pDao = new PeliculaDaoImpl(HibernateUtil.openSession());
-		// Recoge el maximo id
-		String maxId = pDao.searchMaxId();
-		// Comprueba si es una peli manual, para crear el siguiente id
-		if (maxId != null && maxId.startsWith(Constants.PREFIJO_ID_PELIS_MANUALES)) {
-			return Integer.parseInt(maxId) + 1;
-		} else {
-			// Sino, crea el primer id manual
-			return Integer.parseInt(Constants.PREFIJO_ID_PELIS_MANUALES + "00");
-		}
-	}
+  }
 
-	/**
-	 * Genera un nuevo id para las series
-	 * 
-	 * @return Nuevo id
-	 */
-	public static Integer generaSerieId() {
-		SeriesDaoImpl sDao = new SeriesDaoImpl(HibernateUtil.openSession());
-		// Recoge el maximo id
-		String maxId = sDao.searchMaxId();
-		// Comprueba si es una serie manual, para crear el siguiente id
-		if (maxId != null && maxId.startsWith(Constants.PREFIJO_ID_SERIES_MANUALES)) {
-			return Integer.parseInt(maxId) + 1;
-		} else {
-			// Sino, crea el primer id manual
-			return Integer.parseInt(Constants.PREFIJO_ID_SERIES_MANUALES + "00");
-		}
-	}
+  /**
+   * 
+   * Habilita la busqueda de la foto por los ficheros
+   * @param stage
+   * 
+   */
 
-	/**
-	 * Genera un nuevo id para las compañias
-	 * 
-	 * @return Nuevo id
-	 */
-	public static Integer generaCompanyId() {
-		CompañiaDaoImpl cDao = new CompañiaDaoImpl(HibernateUtil.openSession());
-		// Recoge el maximo id
-		String maxId = cDao.searchMaxId();
-		// Comprueba si es una compañia manual, para crear el siguiente id
-		if (maxId != null && maxId.startsWith(Constants.PREFIJO_ID_COMPANIES_MANUALES)) {
-			return Integer.parseInt(maxId) + 1;
-		} else {
-			// Sino, crea el primer id manual
-			return Integer.parseInt(Constants.PREFIJO_ID_COMPANIES_MANUALES + "00");
-		}
-	}
+  public static String buscarFotoArchivos(Stage stage) {
 
-	/**
-	 * Genera un nuevo id para los actores
-	 * 
-	 * @return Nuevo id
-	 */
-	public static Integer generaActorId() {
-		ActoresDaoImpl aDao = new ActoresDaoImpl(HibernateUtil.openSession());
-		// Recoge el maximo id
-		String maxId = aDao.searchMaxId();
-		// Comprueba si es un actor manual, para crear el siguiente id
-		if (maxId != null && maxId.startsWith(Constants.PREFIJO_ID_ACTORES_MANUALES)) {
-			return Integer.parseInt(maxId) + 1;
-		} else {
-			// Sino, crea el primer id manual
-			return Integer.parseInt(Constants.PREFIJO_ID_ACTORES_MANUALES + "00");
-		}
-	}
+    try {
+      FileChooser fileChooser = new FileChooser();
+      fileChooser.setTitle("Buscar Imagen");
+      // Agrega filtros para facilitar la busqueda
+      fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("JPG", "*.jpg"),
+          new FileChooser.ExtensionFilter("PNG", "*.png"),
+          new FileChooser.ExtensionFilter("JFIF", "*.jfif"));
+      // Obtiene la imagen seleccionada
+      return fileChooser.showOpenDialog(stage).getAbsolutePath();
+    } catch (Exception e) {
+      mostrarAlerta("Error al seleccionar una foto", Constants.ERROR_TYPE);
+      return null;
+    }
+  }
 
-	/**
-	 * Genera un nuevo id para los directores
-	 * 
-	 * @return Nuevo id
-	 */
-	public static Integer generaDirectorId() {
-		DirectoresDaoImpl dDao = new DirectoresDaoImpl(HibernateUtil.openSession());
-		// Recoge el maximo id
-		String maxId = dDao.searchMaxId();
-		// Comprueba si es un director manual, para crear el siguiente id
-		if (maxId != null && maxId.startsWith(Constants.PREFIJO_ID_DIRECTORES_MANUALES)) {
-			return Integer.parseInt(maxId) + 1;
-		} else {
-			// Sino, crea el primer id manual
-			return Integer.parseInt(Constants.PREFIJO_ID_DIRECTORES_MANUALES + "00");
-		}
-	}
+  /**
+   * 
+   * Comprueba que la contraseña y la repetición sean iguales
+   *
+   * 
+   * 
+   * @param contrasena
+   * 
+   * @param repeticionContrasena
+   * 
+   * @return True si coinciden; false si son distintas
+   * 
+   */
 
-	/**
-	 * Genera un nuevo id para los generos
-	 * 
-	 * @return Nuevo id
-	 */
-	public static Integer generaGeneroID() {
-		GeneroDaoImpl gDao = new GeneroDaoImpl(HibernateUtil.openSession());
-		// Recoge el maximo id
-		String maxId = gDao.searchMaxId();
-		// Comprueba si es un genero manual, para crear el siguiente id
-		if (maxId != null && maxId.startsWith(Constants.PREFIJO_ID_GENEROS_MANUALES)) {
-			return Integer.parseInt(maxId) + 1;
-		} else {
-			// Sino, crea el primer id manual
-			return Integer.parseInt(Constants.PREFIJO_ID_GENEROS_MANUALES + "00");
-		}
-	}
+  public static boolean compruebaContrasenas(String contrasena, String repeticionContrasena) {
 
+    if (contrasena.compareTo(repeticionContrasena) == 0) {
+      if (contrasena.length() > 0 && contrasena.length() <= 20) {
+        return true;
+      } else {
+        Utils.mostrarAlerta("La contraseña no puede ser mayor de 20 caracteres (ni estar vacía).",
+            Constants.WARNING_TYPE);
+        return false;
+      }
+    } else {
+      Utils.mostrarAlerta("Las constraseñas no coinciden.", Constants.WARNING_TYPE);
+      return false;
+    }
+  }
+
+  /**
+   * 
+   * Muestra una alerta de confirmacion
+   *
+   * 
+   * 
+   * @return True si se pulsa aceptar; False si se pulsa cancelar
+   * 
+   */
+
+  public static boolean confirmacion() {
+    // Crea una alerta de tipo confirmacion
+
+    Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+    alert.setHeaderText(null);
+    alert.setTitle("Confirmación");
+    alert.initOwner(ownerStage);
+    alert.setContentText("¿Estás seguro de querer realizar la acción?");
+
+    // Muestra la alerta y espera hasta que se cierre
+    Optional<ButtonType> result = alert.showAndWait();
+    // Comprueba si se ha pulsado el boton de ok y lo envia
+    return result.isPresent() && result.get() == ButtonType.OK;
+
+  }
+
+  /**
+   * 
+   * Genera un nuevo id para las peliculas
+   *
+   * 
+   * 
+   * @return Nuevo id
+   * 
+   */
+
+  public static Integer generaMovieId() {
+    PeliculaDaoImpl pDao = new PeliculaDaoImpl(HibernateUtil.openSession());
+    // Recoge el maximo id
+    String maxId = pDao.searchMaxId();
+    // Comprueba si es una peli manual, para crear el siguiente id
+    if (maxId != null && maxId.startsWith(Constants.PREFIJO_ID_PELIS_MANUALES)) {
+      return Integer.parseInt(maxId) + 1;
+    } else {
+      // Sino, crea el primer id manual
+      return Integer.parseInt(Constants.PREFIJO_ID_PELIS_MANUALES + "00");
+    }
+
+  }
+
+  /**
+   * 
+   * Genera un nuevo id para las series
+   *
+   * 
+   * 
+   * @return Nuevo id
+   * 
+   */
+
+  public static Integer generaSerieId() {
+    SeriesDaoImpl sDao = new SeriesDaoImpl(HibernateUtil.openSession());
+    // Recoge el maximo id
+    String maxId = sDao.searchMaxId();
+    // Comprueba si es una serie manual, para crear el siguiente id
+    if (maxId != null && maxId.startsWith(Constants.PREFIJO_ID_SERIES_MANUALES)) {
+      return Integer.parseInt(maxId) + 1;
+    } else {
+      // Sino, crea el primer id manual
+      return Integer.parseInt(Constants.PREFIJO_ID_SERIES_MANUALES + "00");
+    }
+  }
+
+  /**
+   * 
+   * Genera un nuevo id para las compañias
+   *
+   * 
+   * 
+   * @return Nuevo id
+   * 
+   */
+
+  public static Integer generaCompanyId() {
+    CompañiaDaoImpl cDao = new CompañiaDaoImpl(HibernateUtil.openSession());
+    // Recoge el maximo id
+    String maxId = cDao.searchMaxId();
+    // Comprueba si es una compañia manual, para crear el siguiente id
+    if (maxId != null && maxId.startsWith(Constants.PREFIJO_ID_COMPANIES_MANUALES)) {
+      return Integer.parseInt(maxId) + 1;
+    } else {
+      // Sino, crea el primer id manual
+      return Integer.parseInt(Constants.PREFIJO_ID_COMPANIES_MANUALES + "00");
+    }
+  }
+
+  /**
+   * 
+   * Genera un nuevo id para los actores
+   *
+   * 
+   * 
+   * @return Nuevo id
+   * 
+   */
+
+  public static Integer generaActorId() {
+    ActoresDaoImpl aDao = new ActoresDaoImpl(HibernateUtil.openSession());
+    // Recoge el maximo id
+    String maxId = aDao.searchMaxId();
+    // Comprueba si es un actor manual, para crear el siguiente id
+    if (maxId != null && maxId.startsWith(Constants.PREFIJO_ID_ACTORES_MANUALES)) {
+      return Integer.parseInt(maxId) + 1;
+    } else {
+      // Sino, crea el primer id manual
+      return Integer.parseInt(Constants.PREFIJO_ID_ACTORES_MANUALES + "00");
+    }
+  }
+
+  /**
+   * 
+   * Genera un nuevo id para los directores
+   *
+   * 
+   * 
+   * @return Nuevo id
+   * 
+   */
+
+  public static Integer generaDirectorId() {
+    DirectoresDaoImpl dDao = new DirectoresDaoImpl(HibernateUtil.openSession());
+    // Recoge el maximo id
+    String maxId = dDao.searchMaxId();
+    // Comprueba si es un director manual, para crear el siguiente id
+    if (maxId != null && maxId.startsWith(Constants.PREFIJO_ID_DIRECTORES_MANUALES)) {
+      return Integer.parseInt(maxId) + 1;
+    } else {
+      // Sino, crea el primer id manual
+      return Integer.parseInt(Constants.PREFIJO_ID_DIRECTORES_MANUALES + "00");
+    }
+  }
+
+  /**
+   * 
+   * Genera un nuevo id para los generos
+   *
+   * 
+   * 
+   * @return Nuevo id
+   * 
+   */
+
+  public static Integer generaGeneroID() {
+    GeneroDaoImpl gDao = new GeneroDaoImpl(HibernateUtil.openSession());
+    // Recoge el maximo id
+    String maxId = gDao.searchMaxId();
+    // Comprueba si es un genero manual, para crear el siguiente id
+    if (maxId != null && maxId.startsWith(Constants.PREFIJO_ID_GENEROS_MANUALES)) {
+      return Integer.parseInt(maxId) + 1;
+    } else {
+      // Sino, crea el primer id manual
+      return Integer.parseInt(Constants.PREFIJO_ID_GENEROS_MANUALES + "00");
+    }
+  }
 }
--- Dorairo/src/test/java/application/AppTest.java
+++ Dorairo/src/test/java/application/AppTest.java
@@ -2,7 +2,6 @@ package application;
 
 import java.io.IOException;
 import java.util.Date;
-
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.testfx.api.FxAssert;
@@ -11,8 +10,8 @@ import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.Start;
 import org.testfx.matcher.base.NodeMatchers;
 import org.testfx.matcher.base.WindowMatchers;
-
 import conexion.HibernateUtil;
+import constants.Constants;
 import dao.UsuarioDaoImpl;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -21,94 +20,197 @@ import javafx.stage.Stage;
 import models.Usuario;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+
 @ExtendWith(ApplicationExtension.class)
 class AppTest {
+  private Stage stage;
+  private Parent mainNode;
+  private Usuario usuarioTest =
+      new Usuario("UsuarioPrueba", "Enrique Martínez", "enrique@ejemplo.com", "1234",
+          new Date());
 
-	private Stage stage;
-	private Parent mainNode;
-	private Usuario usuarioTest = new Usuario("UsuarioPrueba", "Enrique Martínez", "enrique@ejemplo.com",
-			"1234", new Date());
+  @Start
+  public void start(Stage primaryStage) {
+    // Comprueba si existe el usuario para eliminarlo
+    UsuarioDaoImpl uDao = new UsuarioDaoImpl(HibernateUtil.openSession());
+    Usuario user = uDao.searchByUsuario(usuarioTest.getUser());
+    if (user != null) {
+      uDao.delete(user);
 
-	@Start
-	public void start(Stage primaryStage) {
-		// Comprueba si existe el usuario para eliminarlo
-		UsuarioDaoImpl uDao = new UsuarioDaoImpl(HibernateUtil.openSession());
-		if (uDao.searchByUsuario(usuarioTest.getUser()) != null) {
-			uDao.delete(usuarioTest);
-		}
-		stage = primaryStage;
-		try {
-			mainNode = FXMLLoader.load(getClass().getResource("/view/Login.fxml"));
-			primaryStage.setScene(new Scene(mainNode));
-			primaryStage.show();
-			primaryStage.toFront();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    }
 
-	@Test
-	@Order(1)
-	void testUsuarioNoExiste() {
-		// Crea el FxRobot
-		FxRobot fxRobot = new FxRobot();
-		// Introduce un usuario y contraseña de una persona no registrada
-		fxRobot.clickOn("#txtUsuario");
-		fxRobot.write(usuarioTest.getUser());
-		fxRobot.clickOn("#pwContrasena");
-		fxRobot.write(usuarioTest.getClave());
-		// Le da al boton de logearse
-		fxRobot.clickOn("#btnLogin");
-		// Verifica que salta la alerta de error
-		FxAssert.verifyThat("#alertaError", NodeMatchers.isEnabled());
-		// Le da al boton de Aceptar para cerrar la alerta
-		fxRobot.clickOn("#btnAceptar");
-	}
+    stage = primaryStage;
+    try {
+      mainNode = FXMLLoader.load(getClass().getResource("/view/Login.fxml"));
+      primaryStage.setScene(new Scene(mainNode));
+      primaryStage.show();
+      primaryStage.toFront();
+    } catch (IOException e) {
+      e.printStackTrace();
 
-	@Test
-	@Order(2)
-	void testRegistroFunciona() {
-		// Crea el FxRobot
-		FxRobot fxRobot = new FxRobot();
-		// Abre la pantalla de registro
-		fxRobot.clickOn("#lblPulsaAqui");
-		// Introduce los datos
-		fxRobot.clickOn("#txtUsuario");
-		fxRobot.write(usuarioTest.getUser());
-		fxRobot.clickOn("#txtCorreo");
-		fxRobot.write(usuarioTest.getCorreo());
-		fxRobot.clickOn("#txtNombre");
-		fxRobot.write(usuarioTest.getNombre());
-		fxRobot.clickOn("#pwContrasena");
-		fxRobot.write(usuarioTest.getClave());
-		fxRobot.clickOn("#pwRepetirContrasena");
-		fxRobot.write(usuarioTest.getClave());
-		// Presiona el boton de registro
-		fxRobot.clickOn("#btnRegistrarse");
-		// Verifica que salta la alerta de informacion
-		FxAssert.verifyThat("#alertaInformacion", NodeMatchers.isEnabled());
-		// Le da al boton de Aceptar para cerrar la alerta y ventana
-		fxRobot.clickOn("#btnAceptar");
-	}
+    }
 
-	@Test
-	@Order(3)
-	void testUsuarioExiste() {
-		// Crea el FxRobot
-		FxRobot fxRobot = new FxRobot();
-		// Introduce un usuario y contraseña de una persona registrada
-		fxRobot.clickOn("#txtUsuario");
-		fxRobot.write(usuarioTest.getUser());
-		fxRobot.clickOn("#pwContrasena");
-		fxRobot.write(usuarioTest.getClave());
-		// Le da al boton de logearse
-		fxRobot.clickOn("#btnLogin");
-		// Verifica que salta la alerta de informacion
-		FxAssert.verifyThat("#alertaInformacion", NodeMatchers.isEnabled());
-		// Le da al boton de Aceptar para cerrar la alerta
-		fxRobot.clickOn("#btnAceptar");
-		// Comprueba que se abre la ventana de inicio
-		FxAssert.verifyThat(fxRobot.window("Inicio"), WindowMatchers.isShowing());
-	}
+  }
+
+  @Test
+  @Order(1)
+  void testUsuarioNoExiste() {
+    // Crea el FxRobot
+    FxRobot fxRobot = new FxRobot();
+    // Introduce un usuario y contraseña de una persona no registrada
+    fxRobot.clickOn("#txtUser");
+    fxRobot.write(usuarioTest.getUser());
+    fxRobot.clickOn("#pwPassword");
+    fxRobot.write(usuarioTest.getClave());
+    // Le da al boton de logearse
+    fxRobot.clickOn("#btnLogin");
+    // Verifica que salta la alerta de error
+    FxAssert.verifyThat("#alertaError", NodeMatchers.isEnabled());
+    // Le da al boton de Aceptar para cerrar la alerta
+    fxRobot.clickOn("#btnAceptar");
+
+  }
+
+  @Test
+  @Order(2)
+  void testUsuarioExiste() {
+    // Crea el FxRobot
+
+    FxRobot fxRobot = new FxRobot();
+    // Introduce un usuario y contraseña de una persona registrada
+    fxRobot.clickOn("#txtUser");
+    fxRobot.write(".");
+    fxRobot.clickOn("#pwPassword");
+    fxRobot.write(".");
+    // Le da al boton de logearse
+    fxRobot.clickOn("#btnLogin");
+    // Verifica que salta la alerta de informacion
+    FxAssert.verifyThat("#alertaInformacion", NodeMatchers.isEnabled());
+    // Le da al boton de Aceptar para cerrar la alerta
+    fxRobot.clickOn("#btnAceptar");
+    // Comprueba que se abre la ventana de inicio
+    FxAssert.verifyThat(fxRobot.window("Inicio"), WindowMatchers.isShowing());
+
+  }
+
+  @Test
+  @Order(3)
+  void testRegistroCamposVacio() {
+    // Crea el FxRobot
+    FxRobot fxRobot = new FxRobot();
+    // Abre la pantalla de registro
+    fxRobot.clickOn("#lblPulsaAqui");
+    // Introduce los datos
+    fxRobot.clickOn("#txtUsuario");
+    fxRobot.write(usuarioTest.getUser());
+    // Presiona el boton de registro
+    fxRobot.clickOn("#btnRegistrarse");
+    // Verifica que salta la alerta de warning
+    FxAssert.verifyThat("#alertaWarning", NodeMatchers.isEnabled());
+
+  }
+
+  @Test
+  @Order(4)
+  void testRegistroUsuarioYaExistente() {
+    // Crea el FxRobot
+    FxRobot fxRobot = new FxRobot();
+    // Abre la pantalla de registro
+    fxRobot.clickOn("#lblPulsaAqui");
+    // Introduce los datos
+    fxRobot.clickOn("#txtUsuario");
+    fxRobot.write(".");
+    fxRobot.clickOn("#txtCorreo");
+    fxRobot.write(usuarioTest.getCorreo());
+    fxRobot.clickOn("#txtNombre");
+    fxRobot.write(usuarioTest.getNombre());
+    fxRobot.clickOn("#pwContrasena");
+    fxRobot.write(usuarioTest.getClave());
+    fxRobot.clickOn("#pwRepetirContrasena");
+    fxRobot.write(usuarioTest.getClave());
+    // Presiona el boton de registro
+    fxRobot.clickOn("#btnRegistrarse");
+    // Verifica que salta la alerta de warning
+    FxAssert.verifyThat("#alertaWarning", NodeMatchers.isEnabled());
+
+  }
+
+  @Test
+  @Order(5)
+  void testRegistroCorreoYaExistente() {
+    // Crea el FxRobot
+    FxRobot fxRobot = new FxRobot();
+    // Abre la pantalla de registro
+    fxRobot.clickOn("#lblPulsaAqui");
+    // Introduce los datos
+    fxRobot.clickOn("#txtUsuario");
+    fxRobot.write(".");
+    fxRobot.clickOn("#txtCorreo");
+    fxRobot.write(usuarioTest.getCorreo());
+    fxRobot.clickOn("#txtNombre");
+    fxRobot.write(usuarioTest.getNombre());
+    fxRobot.clickOn("#pwContrasena");
+    fxRobot.write(usuarioTest.getClave());
+    fxRobot.clickOn("#pwRepetirContrasena");
+    fxRobot.write(usuarioTest.getClave());
+    // Presiona el boton de registro
+
+    fxRobot.clickOn("#btnRegistrarse");
+    // Verifica que salta la alerta de warning
+    FxAssert.verifyThat("#alertaWarning", NodeMatchers.isEnabled());
+
+  }
+
+  @Test
+  @Order(6)
+  void testRegistroContraseñasIncorrectas() {
+    // Crea el FxRobot
+    FxRobot fxRobot = new FxRobot();
+    // Abre la pantalla de registro
+    fxRobot.clickOn("#lblPulsaAqui");
+    // Introduce los datos
+
+    fxRobot.clickOn("#txtUsuario");
+    fxRobot.write(usuarioTest.getUser());
+    fxRobot.clickOn("#txtCorreo");
+    fxRobot.write(usuarioTest.getCorreo());
+    fxRobot.clickOn("#txtNombre");
+    fxRobot.write(usuarioTest.getNombre());
+    fxRobot.clickOn("#pwContrasena");
+    fxRobot.write(usuarioTest.getClave());
+    fxRobot.clickOn("#pwRepetirContrasena");
+    fxRobot.write("mal");
+    // Presiona el boton de registro
+    fxRobot.clickOn("#btnRegistrarse");
+    // Verifica que salta la alerta de warning
+    FxAssert.verifyThat("#alertaWarning", NodeMatchers.isEnabled());
+
+  }
+
+  @Test
+  @Order(7)
+  void testRegistroFunciona() {
+    // Crea el FxRobot
+    FxRobot fxRobot = new FxRobot();
+    // Abre la pantalla de registro
+    fxRobot.clickOn("#lblPulsaAqui");
+    // Introduce los datos
+    fxRobot.clickOn("#txtUsuario");
+    fxRobot.write(usuarioTest.getUser());
+    fxRobot.clickOn("#txtCorreo");
+    fxRobot.write(usuarioTest.getCorreo());
+    fxRobot.clickOn("#txtNombre");
+    fxRobot.write(usuarioTest.getNombre());
+    fxRobot.clickOn("#pwContrasena");
+    fxRobot.write(usuarioTest.getClave());
+    fxRobot.clickOn("#pwRepetirContrasena");
+    fxRobot.write(usuarioTest.getClave());
+    // Presiona el boton de registro
+    fxRobot.clickOn("#btnRegistrarse");
+    // Verifica que salta la alerta de informacion
+    // FxAssert.verifyThat("#alertaInformacion", NodeMatchers.isEnabled());
+    FxAssert.verifyThat(fxRobot.window(Constants.INFORMATION_TYPE), WindowMatchers.isShowing());
+
+  }
 
 }
